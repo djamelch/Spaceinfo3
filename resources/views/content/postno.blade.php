e9a594b9ec596c5032403344e26d22245eff7a12
chalabi djamel <chalabidjameleddin93@gmail.com>
2018-05-31 02:13:14 +0200

    18 vers

--- resources/views/content/postno.blade.php
+++ resources/views/content/postno.blade.php
@@ -18,7 +18,7 @@
                 
              </tr>
              @foreach($posts as $post)
-             @if($post->accpet===0)
+             @if($post->accpet===0 && public === null)
              <tr>
 
              <th>{{$post->id}}</th>
